1a35ecb6098c742bf52749c0519c556db8879c5e
Op3k <sreeharirathish128@gmail.com>
2025-07-12 09:08:29 +0530

    Initializing Repo

---
+++ server/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen flex justify-center items-center text-2xl font-bold bg-black text-white'>
+      Clean React + <span className='text-blue-700'>TailwindCSS</span>
+    </div>
+  );
+};
+
+export default App;
